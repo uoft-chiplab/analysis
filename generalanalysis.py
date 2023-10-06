0f4ea6f8d1588a0a24ab370db6d7c660ee2d2944
uoft-chiplab <thywissenlab.rpi02@gmail.com>
2023-10-06 09:26:07 -0400

    more help additions

--- generalanalysis.py
+++ generalanalysis.py
@@ -18,16 +18,21 @@ from data import *
 # residuals 
 
 def residuals(filename):
-		fitdata = data(filename)
-		guess = [-0.2, 0, 10, 202]
-		popt, pcov = curve_fit.curve_fit(Cos, fitdata[2], fitdata[3],p0=guess)
-		residuals = fitdata[3] - Cos(fitdata[2],*popt)
-		fig2 = plt.figure(1)
-		plt.plot(fitdata[2],fitdata[3]*0,'-')
-		plt.plot(fitdata[2], residuals, 'g+')
-		plt.xlabel(xlabel)
-		plt.ylabel(ylabel +" Residuals")
-		return fig2
+	"""
+	Inputs: filename
+	
+	Returns: residuals plot 
+	"""
+	fitdata = data(filename)
+	guess = [-0.2, 0, 10, 202]
+	popt, pcov = curve_fit.curve_fit(Cos, fitdata[2], fitdata[3],p0=guess)
+	residuals = fitdata[3] - Cos(fitdata[2],*popt)
+	fig2 = plt.figure(1)
+	plt.plot(fitdata[2],fitdata[3]*0,'-')
+	plt.plot(fitdata[2], residuals, 'g+')
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel +" Residuals")
+	return fig2
 
 
 
