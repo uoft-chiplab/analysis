ffbec1ff2a19420111566d1a4b8801ecfdee31e0
uoft-chiplab <thywissenlab.rpi02@gmail.com>
2024-04-09 13:39:10 -0400

    field wiggle and phase shift data

--- data/FieldWiggleCal/field_cal_summary_analysis.py
+++ data/FieldWiggleCal/field_cal_summary_analysis.py
@@ -8,6 +8,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.optimize import curve_fit
+
+def exponential(x, a, b, c):
+	return a*(1-np.exp(x/b))+c
 
 df = pd.read_csv('field_cal_summary.csv')
 
@@ -18,6 +22,17 @@ ax.errorbar(sub_df['wiggle_freq'], sub_df['B_amp'], sub_df['e_B_amp'],fmt='bo')
 ax.set_xlabel('drive freq (kHz)')
 ax.set_ylabel('B field fit amplitude (G)')
 ax.set_title('Fixed 0.9 Vpp drive amplitude')
+
+xx = sub_df['wiggle_freq']
+yy = sub_df['B_amp']
+yerr = sub_df['e_B_amp']
+guess = [0.07,-10, 0]
+popt, pcov = curve_fit(exponential, xx, yy, sigma=yerr, p0=guess)
+err = np.sqrt(np.diag(pcov))
+num = 500
+xlist = np.linspace(np.min(xx), np.max(xx), num)
+ax.plot(xlist, exponential(xlist, *popt), 'b--')
+ax.plot(xlist, exponential(xlist, *guess), '--', color='orange')
 
 # Fix wiggle_amp at 0.9 Vpp, plot B phase vs. wiggle freq
 fig, ax = plt.subplots(1,1)
